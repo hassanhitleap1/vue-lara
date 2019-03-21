71669d373cb07dd7ab4f5527da14919335622a31
hasan <hassanhitleap1@gmail.com>
2019-03-21 12:43:34 +0300

    lesson 16

---
+++ cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Counter></Counter>
+  </div>
+</template>
+
+<script>
+
+import Counter from './components/Counter';
+
+export default {
+  name: 'app',
+  components: {
+    Counter
+  }
+}
+</script>
+
+
---
+++ cli/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<<template>
+    <div>
+        the count is {{count}}
+        <button class="btn" @click="++count">
+               add +1 
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count:1
+        }
+    },
+}
+</script>
