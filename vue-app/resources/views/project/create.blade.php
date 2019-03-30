59dccede6d57df2c53445604b571c680e2e6b969
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-03-30 07:08:04 -0700

    mm

--- vue-app/resources/views/project/create.blade.php
+++ vue-app/resources/views/project/create.blade.php
@@ -12,42 +12,38 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="name" type="text" class="form-control is-invalid" name="name" required autofocus>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong></strong>
+                                </span>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('description') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="description" type="text" class="form-control is-invalid" name="description" required>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong></strong>
+                                </span>
 
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
                             </div>
                         </div>
 
-                      
+
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Save') }}
                                 </button>
 
-                                
+
                             </div>
                         </div>
                     </form>
@@ -56,4 +52,16 @@
         </div>
     </div>
 </div>
-@endsection
+
+<script>
+    class Error {
+        constructor(errors) {
+            this.errors = errors;
+        }
+    }
+    const app = new Vue({
+        el: '#app'
+    });
+</script>
+
+@endsection 
